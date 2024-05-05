eac917e99cbe28689ef3b685b9417c7ac2d715c6
mingoo <83053171+panggu15@users.noreply.github.com>
2024-05-05 21:21:59 +0900

    Update masactrl.py

--- masactrl/masactrl.py
+++ masactrl/masactrl.py
@@ -67,8 +67,14 @@ class MutualSelfAttentionControl(AttentionBase):
 
         out_u = self.attn_batch(qu, ku[:num_heads], vu[:num_heads], sim[:num_heads], attnu, is_cross, place_in_unet, num_heads, **kwargs)
         out_c = self.attn_batch(qc, kc[:num_heads], vc[:num_heads], sim[:num_heads], attnc, is_cross, place_in_unet, num_heads, **kwargs)
-        out = torch.cat([out_u[:1], out_u[:1], out_c[:1], out_c[:1]], dim=0)
+        out = torch.cat([out_u, out_c], dim=0)
 
+
+        out_u1 = self.attn_batch(qu, ku[:num_heads], vu[:num_heads], sim[:num_heads], attnu, is_cross, place_in_unet, num_heads, **kwargs)
+        out_u2 = self.attn_batch(qu, ku[:num_heads], vu[num_heads:], sim[:num_heads], attnu, is_cross, place_in_unet, num_heads, **kwargs)
+        out_c1 = self.attn_batch(qc, kc[:num_heads], vc[:num_heads], sim[:num_heads], attnc, is_cross, place_in_unet, num_heads, **kwargs)
+        out_c2 = self.attn_batch(qc, kc[:num_heads], vc[num_heads:], sim[:num_heads], attnc, is_cross, place_in_unet, num_heads, **kwargs)
+        out = torch.cat([out_u1, out_u2, out_c1, out_c2], dim=0)
         # v[16:] = v[:16]
         # out = self.attn_batch(q, k, v, sim, attnc, is_cross, place_in_unet, num_heads, **kwargs)
         # out = torch.einsum("h i j, h j d -> h i d", attn, v)
